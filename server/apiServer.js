48c4ee1f92ba73381bdf26aec56b9d7b42b0264e
Utsav Shrestha <12119960@cqumail.com>
2021-06-01 08:31:56 +1000

    initial commit

---
+++ server/apiServer.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const uri =
+  'mongodb+srv://admin:Nepal@123@cluster0.bnb9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const app = express();
+let corsOptions = {
+  origin: '*',
+  method: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+//app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = 3000;
+const database = 'cqu_assignment2';
+const docs = 'dogbase';
+
+// Global for general use
+let currCollection;
+
+client.connect((err) => {
+  currCollection = client.db(database).collection(docs);
+  // perform actions on the collection object
+  console.log('Database up!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/getData', cors(corsOptions), (req, res) => {
+  currCollection
+    .find()
+    .toArray()
+    .then((results) => {
+      res.setHeader('content-type', 'application/json');
+      res.send(results);
+    })
+    .catch((error) => console.error(error));
+});
+
+app.post('/saveDoc', cors(corsOptions), (req, res) => {
+  console.log(`request body: ${JSON.stringify(req.body)}`);
+  currCollection
+    .insertOne(req.body)
+    .then((result) => {
+      res.setHeader('content-type', 'application/json');
+      res.send(results);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+});
+
+app.post('/saveDocs', cors(corsOptions), (req, res) => {
+  console.log(`request body: ${JSON.stringify(req.body.foo)}`);
+  let data = JSON.parse(req.body.foo);
+  currCollection
+    .insertMany(data)
+    .then((result) => {
+      res.setHeader('content-type', 'application/json');
+      res.send(result);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.setHeader('content-type', 'application/json');
+      res.send(error);
+    });
+});
+
+app.delete('/deleteAllDocs', cors(corsOptions), (req, res) => {
+  currCollection
+    .deleteMany({})
+    .then((result) => {
+      res.setHeader('content-type', 'application/json');
+      res.send(result);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.setHeader('content-type', 'application/json');
+      res.send(error);
+    });
+});
+
+async function run(docs) {
+  try {
+    await client.connect();
+    const db = client.db(database);
+    let currCollection = db.collection(docs);
+
+    const query = { name: 'Ribeira Charming Duplex' };
+    const cursor = currCollection.find();
+    const data = await cursor.toArray();
+    console.log(data);
+    return data;
+  } finally {
+    //await client.close();
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
