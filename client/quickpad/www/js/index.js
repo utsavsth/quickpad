ee39dcb476d3bdb9e5c629ce1e5c26f129f483f0
Utsav Shrestha <12119960@cqumail.com>
2021-06-09 00:53:44 +1000

    login and update notes

--- client/quickpad/www/js/index.js
+++ client/quickpad/www/js/index.js
@@ -1,32 +1,52 @@
-const serverURL = 'http://172.20.10.2:3000';
+const serverURL = 'http://192.168.1.106:3000';
 const getData = `${serverURL}/getData`;
 const saveData = `${serverURL}/saveDoc`;
 const uploadData = `${serverURL}/saveDocs`;
 const deleteData = `${serverURL}/deleteAllDocs`;
 let dataArray = [];
+let noteId;
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
 class Note {
-  constructor(title, note) {
+  constructor(title, note, username) {
     this.title = title;
     this.content = note;
-    this.author = 'utsav';
+    this.author = username;
     this.createdDt = new Date();
     this.modifiedDt = new Date();
     this.sharableLink = '';
     this.isDeleted = false;
   }
 }
-let getDataFromCloud = async () => {
-  let promise = new Promise((res, rej) => {
+
+let getAllDataFromCloud = async (username, searchText) => {
+  let promise = new Promise((resolve, reject) => {
     try {
-      $.get(getData, function (data, status) {
-        console.log('inside await fn');
-        res(data);
+      let isLoggedin = localStorage.getItem('username');
+      if (isLoggedin === undefined || isLoggedin === null) {
+        reject({ status: 403, msg: 'not logged in!' });
+        return;
+      }
+
+      let queryUrl = `${getData}`;
+      queryUrl = queryUrl.concat('?search=' + (searchText || ''));
+      console.log(queryUrl);
+      $.ajax({
+        url: queryUrl,
+        type: 'GET',
+        headers: {
+          'X-Requested-Username': username,
+        },
+        success: function (data, status) {
+          resolve(data);
+        },
+        error: function (error) {
+          reject(error);
+        },
       });
-    } catch (error) {
-      rej(error);
+    } catch (err) {
+      reject(err);
     }
   });
 
@@ -35,22 +55,34 @@ let getDataFromCloud = async () => {
   return result;
 };
 
-let saveDataToCloud = async (notes) => {
-  let promise = new Promise((res, rej) => {
+let getDatabyIdFromCloud = async (username, id) => {
+  let promise = new Promise((resolve, reject) => {
     try {
-      var str = JSON.stringify(notes);
-      $.post(
-        saveData,
-        { foo: str },
-        function (data, status) {
-          if (status === 'success') {
-            res(data);
-          }
+      let isLoggedin = localStorage.getItem('username');
+      if (isLoggedin === undefined || isLoggedin === null) {
+        reject({ status: 403, msg: 'not logged in!' });
+        return;
+      }
+
+      let queryUrl = `${getData}`;
+      queryUrl = queryUrl.concat('/' + id);
+      console.log(queryUrl);
+      $.ajax({
+        url: queryUrl,
+        type: 'GET',
+        headers: {
+          'X-Requested-Username': username,
         },
-        'json'
-      );
-    } catch (error) {
-      rej(error);
+        success: function (data, status) {
+          resolve(data);
+        },
+        error: function (error) {
+          reject(error);
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      reject(err);
     }
   });
 
@@ -58,7 +90,66 @@ let saveDataToCloud = async (notes) => {
   return result;
 };
 
-let fetchNotes = async () => {
+let saveDataInCloud = async (notes) => {
+  let promise = new Promise((resolve, reject) => {
+    try {
+      var str = JSON.stringify(notes);
+      $.post(
+        saveData,
+        { foo: str },
+        function (data, status) {
+          if (status === 'success') {
+            resolve(data);
+          }
+        },
+        'json'
+      );
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+  let result = await promise;
+  return result;
+};
+
+let updateDataInCloud = async (id, title, content) => {
+  let promise = new Promise((resolve, reject) => {
+    try {
+      let username = localStorage.getItem('username');
+      var str = JSON.stringify({ title, content });
+      var putUrl = saveData + `/${id}`;
+      $.ajax({
+        url: putUrl,
+        type: 'PUT',
+        headers: {
+          'X-Requested-Username': username,
+        },
+        data: { foo: str },
+        success: function (data, status) {
+          resolve(data);
+        },
+        error: function (error) {
+          reject(error);
+        },
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+  let result = await promise;
+  return result;
+};
+
+let fetchNotes = async (username, searchText) => {
+  if (username === undefined || username === null) {
+    username = localStorage.getItem('username');
+    if (username === undefined || username === null) {
+      console.log('not logged in');
+      return;
+    }
+  }
   let outputDataTable = function (data) {
     let html = '';
     let htmlSegment = '';
@@ -71,29 +162,50 @@ let fetchNotes = async () => {
       htmlSegment += '<table class="ui-responsive table-stroke"><tbody>';
 
       htmlSegment += '<tr class="col-id"><td class="col-header">Id</td>';
-      htmlSegment += '<td>' + element['_id'] + '</td></tr>';
+      htmlSegment += '<td colspan="3">' + element['_id'] + '</td></tr>';
       htmlSegment += '<tr><td class="col-header">Title</td>';
-      htmlSegment += '<td>' + element['title'] + '</td></tr>';
-
+      htmlSegment += '<td>' + element['title'] + '</td>';
+      htmlSegment +=
+        '<td><a href="#" id="e_' +
+        element['_id'] +
+        '" class="edit-note ui-btn ui-icon-edit ui-btn-icon-left ui-btn-icon-notext ui-shadow ui-corner-all">edit</a></td>';
+      htmlSegment +=
+        '<td><a href="#" id="d_' +
+        element['_id'] +
+        '" class="del-note ui-btn ui-icon-delete ui-btn-icon-left ui-btn-icon-notext ui-shadow ui-corner-all">delete</a></td></tr>';
       htmlSegment += '</tbody></table>';
+
       htmlSegment += '</li>';
       html += htmlSegment;
     });
     $('#notes-container ul').append(html);
     $('#notes-container ul').listview('refresh');
   };
-  await getDataFromCloud().then(function (response) {
-    data = response;
-    outputDataTable(data);
-  });
+  await getAllDataFromCloud(username, searchText).then(
+    function (response) {
+      data = response;
+      $('#notes-container ul').html('');
+      outputDataTable(data);
+    },
+    function (err) {
+      debugger;
+    }
+  );
 };
 
 let saveNote = async (notes) => {
-  await saveDataToCloud(notes).then(function (response) {
-    debugger;
+  await saveDataInCloud(notes).then(function (response) {
     console.log(response);
     clearForm();
     alert('Data Stored!');
+    fetchNotes();
+  });
+};
+
+let updateNote = async (id, title, note) => {
+  await updateDataInCloud(id, title, note).then(function (response) {
+    console.log(response);
+    alert('Data Updated!');
     fetchNotes();
   });
 };
@@ -103,14 +215,61 @@ let clearForm = function () {
   $('form#note-form').validate().resetForm();
 };
 
+$(document).on('pagehide', '#pg-display-new', function (evt) {
+  noteId = '';
+  $('#editNote').val('');
+});
+
 $(document).on('pagecreate', '#mainPage', function (event) {
   let page = $(this);
-  fetchNotes();
+  let username = localStorage.getItem('username');
+  let typingInterval = 3000; //3000ms = 3sec
+  let typingTimer;
+
+  if (username === undefined || username === null) {
+    console.log('not logged in');
+    window.location.href = 'index.html';
+  }
+  fetchNotes(username, '');
 
   $(page).on('click', '#addLocalData', function (evt) {
     $('body').pagecontainer('change', '#pg-display-new', {
       transition: 'flip',
     });
+  });
+
+  $(document).on('keyup', '#searchNotes', function (e) {
+    console.log(e.target.value);
+    e.preventDefault();
+    //searching after user is done typing
+    clearTimeout(typingTimer);
+    if (e.target.value) {
+      typingTimer = setTimeout(function () {
+        //async call to get searching notes
+        fetchNotes(username, e.target.value);
+      }, typingInterval);
+    }
+  });
+
+  $(document).on('click', '.edit-note', function (e) {
+    console.log(e.target.id);
+    e.preventDefault();
+    getDatabyIdFromCloud(
+      username,
+      e.target.id.substring(2, e.target.id.length)
+    ).then(function (response) {
+      var note = response[0];
+      noteId = note['_id'];
+      $('#editNote').val(note['content']);
+      $('body').pagecontainer('change', '#pg-display-new', {
+        transition: 'flip',
+      });
+    });
+  });
+
+  $(document).on('click', '.del-note', function (e) {
+    debugger;
+    console.log(e.target.id);
   });
 
   $(document).on('click', '#btnSave', function (evt) {
@@ -120,8 +279,12 @@ $(document).on('pagecreate', '#mainPage', function (event) {
       let note = $('#editNote').val();
       let title = note.substring(0, 30);
 
-      let myNote = new Note(title, note);
-      saveNote(myNote);
+      if (noteId !== null && noteId !== '' && noteId.length > 0) {
+        updateNote(noteId, title, note);
+      } else {
+        let myNote = new Note(title, note, username);
+        saveNote(myNote);
+      }
     } else {
       $('#errorMsg').popup('open');
     }
@@ -132,7 +295,6 @@ $(document).on('pagecreate', '#mainPage', function (event) {
 function onDeviceReady() {
   console.log('device is ready!');
   const getStartPosition = (e) => {
-    debugger;
     const delta_x = e.deltaX;
     const delta_y = e.deltaY;
     const final_x = e.srcEvent.pageX || e.srcEvent.screenX || 0;
@@ -147,7 +309,6 @@ function onDeviceReady() {
   $('#notes-container ul li').each(function () {
     var mc = new Hammer(this);
     mc.on('swipeleft', function (e) {
-      debugger;
       var id = e.target.id;
       const { x } = getStartPosition(e);
       if (x >= 0 && x <= 50) {
--- server/apiServer.js
+++ server/apiServer.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const uri =
   'mongodb+srv://admin:admin@cluster0.x7lvb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
@@ -14,8 +14,11 @@ let corsOptions = {
   origin: '*',
   method: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
+  headers: 'Origin, X-Requested-With, Content-Type, Accept, X-Test-Header',
   optionsSuccessStatus: 204,
 };
+app.use(cors());
+
 //app.use(cors);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -31,15 +34,63 @@ client.connect((err) => {
   currCollection = client.db(database).collection(docs);
   // perform actions on the collection object
   console.log('Database up!');
+  console.log(new Date());
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/redirect', (req, res) => {
+  //res.setHeader('X-Redirect', 'notes.html');
+
+  res.set('X-Redirect', 'notes.html');
+  res.header(
+    'Access-Control-Expose-Headers',
+    'Content-Type, Location, X-Redirect'
+  );
+  res.status(200).send('send');
 });
 
-app.get('/getData', cors(corsOptions), (req, res) => {
+app.get('/getData', (req, res) => {
+  let searchText = req.query.search || '';
+  let requestHeaders = req.headers;
+  console.log(new Date());
+  var user = req.get('x-requested-username');
+
+  const query = {
+    $and: [
+      {
+        $or: [
+          { title: { $regex: '.*' + searchText + '.*' } },
+          { content: { $regex: '.*' + searchText + '.*' } },
+        ],
+      },
+      { author: user },
+    ],
+  };
+
   currCollection
-    .find()
+    .find(query)
+    .toArray()
+    .then((results) => {
+      res.setHeader('content-type', 'application/json');
+      //res.setHeader('Access-Control-Allow-Origin', '*');
+      //res.setHeader(
+      //  'Access-Control-Allow-Headers',
+      // 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, X-Test-Header, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+      //);
+      res.send(results);
+    })
+    .catch((error) => console.error(error));
+});
+
+app.get('/getData/:id', (req, res) => {
+  var user = req.get('x-requested-username');
+  var id = req.params.id;
+  console.log(id);
+  const query = {
+    $and: [{ _id: ObjectId(id) }, { author: user }],
+  };
+
+  currCollection
+    .find(query)
     .toArray()
     .then((results) => {
       res.setHeader('content-type', 'application/json');
@@ -69,6 +120,31 @@ app.post('/saveDocs', cors(corsOptions), (req, res) => {
   let data = JSON.parse(req.body.foo);
   currCollection
     .insertMany(data)
+    .then((result) => {
+      res.setHeader('content-type', 'application/json');
+      res.send(result);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.setHeader('content-type', 'application/json');
+      res.send(error);
+    });
+});
+
+app.put('/saveDoc/:id', (req, res) => {
+  var user = req.get('x-requested-username');
+  var id = req.params.id;
+  let data = JSON.parse(req.body.foo);
+  console.log('put method: ' + id);
+  const filter = {
+    $and: [{ _id: ObjectId(id) }, { author: user }],
+  };
+  const update = {
+    $set: { title: data.title, content: data.content, modifiedDt: new Date() },
+  };
+
+  currCollection
+    .update(filter, update)
     .then((result) => {
       res.setHeader('content-type', 'application/json');
       res.send(result);
